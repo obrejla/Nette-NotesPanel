6671883ddd7378d1b3d82d2b820632d9bbb01b4a
Ondřej Brejla <ondrej@brejla.cz>
2010-05-22 19:56:57 +0200

    License texts and PHP doc comments.

--- NotesPanel.php
+++ NotesPanel.php
@@ -1,9 +1,34 @@
 <?php
 
 /**
- * Description of NotesPanel
+ * Copyright (c) 2010 Ondřej Brejla <ondrej@brejla.cz>
  *
- * @author Ondřej Brejla
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * Panel for the Nette DebugBar. It allows you to write notes for each site of your web pages.
+ *
+ * @author Ondřej Brejla <ondrej@brejla.cz>
  */
 class NotesPanel extends Control implements IDebugPanel {
 
@@ -128,7 +153,7 @@ class NotesPanel extends Control implements IDebugPanel {
 	 *
 	 * @return INotesPanelModel
 	 */
-	public function getModel() {
+	private function getModel() {
 		if (is_null($this->model)) {
 			$this->model = new NotesPanelModel();
 		}
@@ -150,7 +175,7 @@ class NotesPanel extends Control implements IDebugPanel {
 	 *
 	 * @return IHttpRequest
 	 */
-	public function getHttpRequest() {
+	private function getHttpRequest() {
 		if (is_null($this->httpRequest)) {
 			throw new InvalidStateException('No HttpRequest. HttpRequest was not set.');
 		}
